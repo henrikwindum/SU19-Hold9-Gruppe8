2e16749cf64b0fbdbb7b44b1328d3f5819767326
Henrik Windum <henrikwindum95@gmail.com>
2019-03-19 11:04:15 +0100

    GalagaStates?

---
+++ SU19-Exercises/Galaga-Exercise-3/GalagaStates/StateTransformer.cs
@@ -0,0 +1,11 @@
+namespace Galaga_Exercise_3.GalagaStates {
+    public class StateTransformer {
+        public static GameStateType TransformStringToState(string state) {
+            
+        }
+
+        public static string TransformStateToString(GameStateType state) {
+            
+        }
+    }
+}
